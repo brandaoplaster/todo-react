b1e1f50eb36f68bff44682946e299bc9971df3cf
brandaoplaster <brandaoplaster@gmail.com>
2025-11-02 19:30:25 -0300

    feat: Update button icon component use skeleton

--- src/components/button-icon.tsx
+++ src/components/button-icon.tsx
@@ -1,20 +1,21 @@
-import type React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 import Icon from "./icon";
+import Skeleton from "./skeleton";
 
 export const buttonIconVariants = cva(
   `
-   inline-flex items-center justify-center cursor-pointer transition group
-  `,
+  inline-flex items-center justify-center cursor-pointer transition group
+`,
   {
     variants: {
       variant: {
+        none: "",
         primary: "bg-green-base hover:bg-green-dark",
         secondary: "bg-gray-200 hover:bg-pink-base",
         tertiary: "bg-transparent hover:bg-gray-200",
       },
       size: {
-        sm: "w-6 h-6 rounded",
+        sm: "w-6 h-6 p-1 rounded",
       },
       disabled: {
         true: "opacity-50 pointer-events-none",
@@ -28,48 +29,55 @@ export const buttonIconVariants = cva(
   }
 );
 
-export const buttonIconIconVariants = cva("transition", {
-  variants: {
-    variant: {
-      primary: "fill-white",
-
-      secondary: "fill-pink-base group-hover:fill-white",
-
-      tertiary: "fill-gray-300 group-hover:fill-gray-400",
-    },
-
-    size: {
-      sm: "w-4 h-4",
-    },
-  },
-
-  defaultVariants: {
-    variant: "primary",
-
-    size: "sm",
-  },
-});
-
 interface ButtonIconProps
   extends Omit<React.ComponentProps<"button">, "disabled" | "size">,
     VariantProps<typeof buttonIconVariants> {
   icon: React.ComponentProps<typeof Icon>["svg"];
+  loading?: boolean;
 }
+
+export const buttonIconIconVariants = cva("transition", {
+  variants: {
+    variant: {
+      none: "",
+      primary: "fill-white",
+      secondary: "fill-pink-base group-hover:fill-white",
+      tertiary: "fill-gray-300 group-hover:fill-gray-400",
+    },
+    size: {
+      sm: "w-4 h-4",
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+    size: "sm",
+  },
+});
 
 export default function ButtonIcon({
   variant,
   size,
   disabled,
   className,
+  loading,
   icon,
-  ...propos
+  ...props
 }: ButtonIconProps) {
+  if (loading) {
+    return (
+      <Skeleton
+        rounded="sm"
+        className={buttonIconVariants({ variant: "none", size, className })}
+      />
+    );
+  }
+
   return (
     <button
-      className={buttonIconVariants({ variant, size, disabled, className })}
-      {...propos}
+      className={buttonIconVariants({ className, variant, size, disabled })}
+      {...props}
     >
-      <Icon className={buttonIconIconVariants({ variant, size })} svg={icon} />
+      <Icon className={buttonIconIconVariants({ size, variant })} svg={icon} />
     </button>
   );
 }
