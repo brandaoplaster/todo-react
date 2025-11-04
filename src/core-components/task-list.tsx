493365086bcc8c9a83a166445790a6dd2faa408d
brandaoplaster <brandaoplaster@gmail.com>
2025-11-04 16:29:52 -0300

    feat: Add dynamic task listing

--- src/core-components/task-list.tsx
+++ src/core-components/task-list.tsx
@@ -2,6 +2,7 @@ import Button from "../components/button";
 import PlusIcon from "../assets/icons/plus.svg?react";
 import TaskItem from "./task-item";
 import useTasks from "../hooks/use-tasks";
+import { TaskState } from "../types/task";
 
 export default function TaskList() {
   const { tasks, prepareTask } = useTasks();
@@ -13,16 +14,20 @@ export default function TaskList() {
   return (
     <>
       <section>
-        <Button icon={PlusIcon} className="w-full" onClick={handleNewTask}>
+        <Button
+          icon={PlusIcon}
+          className="w-full"
+          onClick={handleNewTask}
+          disabled={tasks.some((task) => task.state === TaskState.Creating)}
+        >
           New Task
         </Button>
       </section>
 
       <section className="space-y-2">
-        <TaskItem />
-        <TaskItem />
-        <TaskItem />
-        <TaskItem />
+        {tasks.map((task) => (
+          <TaskItem key={task.id} task={task} />
+        ))}
       </section>
     </>
   );
